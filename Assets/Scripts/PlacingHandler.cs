4cb3933a119855adb712d2f9defd121c30bacb3d
Elle Adali <eliana.adali06@gmail.com>
2026-01-31 16:54:24 -0500

    placing, works but no queue yet

--- Assets/Scripts/PlacingHandler.cs
+++ Assets/Scripts/PlacingHandler.cs
@@ -22,13 +22,15 @@ public class PlacingHandler : MonoBehaviour
         {
             Vector2 mousePos = Mouse.current.position.ReadValue();
             Vector3 worldCoords = Camera.main.ScreenToWorldPoint(mousePos);
+            worldCoords.z = 0;
             Vector3Int cellCoords = tilemap.WorldToCell(worldCoords);
             
             TileBase tile = tilemap.GetTile(cellCoords);
             if (tile != null)
             {
                 GameObject animal = testAnimal;
-                Instantiate(animal, worldCoords, Quaternion.identity);
+                GameObject clone = Instantiate(animal, worldCoords, Quaternion.identity);
+                Debug.Log("spawned at " + clone.transform.position);
             }
         }
     }
