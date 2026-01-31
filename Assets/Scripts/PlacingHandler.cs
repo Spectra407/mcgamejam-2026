b042d4ee3dee5f242a9035a1cc3408e1e60e4fe9
Elle Adali <eliana.adali06@gmail.com>
2026-01-31 16:33:29 -0500

    add placing handler but it doesnt work yet

---
+++ Assets/Scripts/PlacingHandler.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
+
+public class PlacingHandler : MonoBehaviour
+{
+    private Tilemap tilemap;
+    [SerializeField] private Queue queue;
+    private InputAction placeAction;
+
+    public GameObject testAnimal;
+
+    void Start()
+    {
+        tilemap = gameObject.GetComponent<Tilemap>();
+        placeAction = InputSystem.actions.FindAction("Click");
+    }
+
+    void Update()
+    {
+        if (placeAction.WasPressedThisFrame())
+        {
+            Vector2 mousePos = Mouse.current.position.ReadValue();
+            Vector3 worldCoords = Camera.main.ScreenToWorldPoint(mousePos);
+            Vector3Int cellCoords = tilemap.WorldToCell(worldCoords);
+            
+            TileBase tile = tilemap.GetTile(cellCoords);
+            if (tile != null)
+            {
+                GameObject animal = testAnimal;
+                Instantiate(animal, worldCoords, Quaternion.identity);
+            }
+        }
+    }
+}
