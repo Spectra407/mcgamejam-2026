eb3a994d8521fb0f85bfd356780788baefb3283c
joshuacoughler <joshuacoughlerbusiness@gmail.com>
2026-02-01 10:04:13 -0500

    asdadadad
    asdasdasd

--- Assets/Scripts/Scoreboard.cs
+++ Assets/Scripts/Scoreboard.cs
@@ -44,7 +44,7 @@ public class Scoreboard : MonoBehaviour
         score += totalCount * lowestCount;
     }
 
-    private void EndOfGameScore()
+    private string EndOfGameScore()
     {
        Debug.Log("Game Over\n" + "Your Score Was: " + score.ToString("000000"));
     }
