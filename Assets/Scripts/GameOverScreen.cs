9218e579fb9f9f26d179c5949d11c9d20ced29ef
joshuacoughler <joshuacoughlerbusiness@gmail.com>
2026-02-01 11:25:37 -0500

    asdadasd
    asdasdasd

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -13,9 +13,16 @@ public class GameOverScreen : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-        gameOverScore.text = scoreboard.endOfGameScore;
+        gameOverScore.SetActive(false);
+        StartCoroutine(ShowText());
 
+    }
+
+    public IEnumerator ShowText()
+    {
+        yield return new WaitForSeconds(5);
+        gameOverScore.text = scoreboard.endOfGameScore;
+        gameOverScore.SetActive(true);
     }
 
     // Update is called once per frame
