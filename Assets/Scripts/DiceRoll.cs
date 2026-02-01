7f0448916162a149ca209359694c941799eee9d6
Nomaan Arshad <159752966+nomaan-arshad@users.noreply.github.com>
2026-02-01 09:36:22 -0500

    dice roll script

---
+++ Assets/Scripts/DiceRoll.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceRoll : MonoBehaviour
+{
+    
+    void Start()
+    {
+        //gameObject.GetComponent<Button>().onClick.AddListener(
+        //    () => //play the video here (type));
+
+
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
